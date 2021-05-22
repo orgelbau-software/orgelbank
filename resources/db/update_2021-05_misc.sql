db9196a6bb6bb3b0bc7a4650d1c5f8d6d67f5be2
Stephan Watermeyer <m@phrk.de>
2021-05-22 20:01:28 +0200

    Fehlendes SQL fuer den Cronjob eingefuegt

--- resources/db/update_2021-05_misc.sql
+++ resources/db/update_2021-05_misc.sql
@@ -10,3 +10,5 @@ INSERT INTO `option_meta` (`option_id`, `option_modul`, `option_name`, `option_v
 ALTER TABLE `rechnung_position` ADD `rpos_type` INT NOT NULL AFTER `rpos_id`;
 ALTER TABLE `arbeitswoche` ADD `aw_stunden_urlaub` INT NULL DEFAULT NULL AFTER `aw_stunden_dif`;
 ALTER TABLE `arbeitstag` ADD `aw_id` INT NULL DEFAULT NULL AFTER `at_id`;
+
+INSERT INTO `option_meta` (`option_id`, `option_modul`, `option_name`, `option_value`, `option_autoload`, `option_comment`, `option_editable`, `option_lastchange`, `option_createdate`) VALUES (NULL, 'Einstellung', 'cronjob_geostatus_limit', '30', '1', 'Wieviele Adressen der GeoStatus Cronjob pro Lauf verarbeiten soll', '1', '', '');
