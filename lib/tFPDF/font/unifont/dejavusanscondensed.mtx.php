0638d05d50970919a7c3543a20ddb3a556ba4b6e
Stephan Watermeyer <m@phrk.de>
2021-10-10 20:19:23 +0200

    Fonts for FPDF

--- lib/tFPDF/font/unifont/dejavusanscondensed.mtx.php
+++ lib/tFPDF/font/unifont/dejavusanscondensed.mtx.php
@@ -2,18 +2,18 @@
 $name='DejaVuSansCondensed';
 $type='TTF';
 $desc=array (
-  'Ascent' => 928,
-  'Descent' => -236,
-  'CapHeight' => 928,
+  'Ascent' => 928.0,
+  'Descent' => -236.0,
+  'CapHeight' => 928.0,
   'Flags' => 4,
   'FontBBox' => '[-918 -415 1513 1167]',
-  'ItalicAngle' => 0,
-  'StemV' => 87,
-  'MissingWidth' => 540,
+  'ItalicAngle' => 0.0,
+  'StemV' => 87.0,
+  'MissingWidth' => 540.0,
 );
 $up=-63;
 $ut=44;
-$ttffile='C:/dev/app-server/xampp/htdocs/krawinkel.watermeyer.info/lib/tFPDF/font/unifont/DejaVuSansCondensed.ttf';
+$ttffile='C:\dev\app-server\xampp-2021\htdocs\krawinkel.watermeyer.info\lib\tFPDF/font/unifont/DejaVuSansCondensed.ttf';
 $originalsize=643852;
 $fontkey='dejavu';
 ?>
