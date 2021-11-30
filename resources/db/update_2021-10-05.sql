d70e1860b14a424ca6b0c93748a27a80b7487bac
Stephan Watermeyer <m@phrk.de>
2021-11-30 22:47:29 +0100

    Feature: Projekt Nebenkosten

--- resources/db/update_2021-10-05.sql
+++ resources/db/update_2021-10-05.sql
@@ -15,11 +15,8 @@ ALTER TABLE `ansprechpartner` ADD `a_webseite` VARCHAR(200) NULL AFTER `a_aktiv`
 ALTER TABLE `ansprechpartner` CHANGE `a_bemerkung` `a_bemerkung` VARCHAR(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
 
 CREATE TABLE `nebenkosten_rechnung` ( `nk_id` int(10) NOT NULL, `proj_id` int(10) NOT NULL, `be_id` int(11) NOT NULL, `nk_nummer` varchar(100) NOT NULL, `nk_datum` date NOT NULL, `nk_kommentar` text NOT NULL, `nk_betrag` double(10,2) NOT NULL, `nk_lieferant` varchar(100) NOT NULL, `nk_leistung` varchar(100) NOT NULL, `nk_lastchange` datetime NOT NULL, `nk_createdate` datetime NOT NULL ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-ALTER TABLE `nebenkosten_rechnung`
-  ADD PRIMARY KEY (`nk_id`);
+ALTER TABLE `nebenkosten_rechnung`  ADD PRIMARY KEY (`nk_id`);
 
-ALTER TABLE `nebenkosten_rechnung`
-  MODIFY `nk_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
+ALTER TABLE `nebenkosten_rechnung` MODIFY `nk_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 INSERT INTO `option_meta` (`option_id`, `option_modul`, `option_name`, `option_value`, `option_autoload`, `option_comment`, `option_editable`, `option_lastchange`, `option_createdate`) VALUES (NULL, 'Projekt', 'nebenkosten', 'LKW,PKW,Anhänger,Spesen,Hotel', '1', 'Standard Nebenkosten Auswahl ', '1', '2021-11-30 22:05:34.000000', '2021-11-30 22:05:34.000000');
