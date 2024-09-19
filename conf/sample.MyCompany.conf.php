64d6db7e4bca6633b5fe4e9a128e3d2cc7da3b00
Stephan Watermeyer <m@phrk.de>
2024-09-19 17:16:36 +0200

    Added Example Config. Add config.inc.php to gitignore

--- conf/sample.MyCompany.conf.php
+++ conf/sample.MyCompany.conf.php
@@ -1,7 +1,7 @@
 <?php
 define('INSTALLATION_NAME', 'BEISPIEL');
 
-define('MYSQL_HOST', 'localhost');
+define('MYSQL_HOST', '');
 define('MYSQL_USER', '');
 define('MYSQL_PASS', '');
 define('MYSQL_DB', '');
@@ -9,15 +9,14 @@ define('MYSQL_DB', '');
 define('ROOTDIR', $_SERVER['DOCUMENT_ROOT']);
 define('TMPDIR', ROOTDIR . "/tmp/");
 define('BACKUPDIR', ROOTDIR . "backup/");
-define('INSTANCE_URL', "http://localhost/");
+define('INSTANCE_URL', "https://localhost/");
 
 define('SUPPORT_MAIL_ENABLED', true);
 define('GOOGLE_MAPS_API_KEY', '');
-define('ORGELBANK_API_KEY', 'AMGks2xFxDrVsGs6');
+define('ORGELBANK_API_KEY', '');
 
 define('LOGENABLED', false);
-define('SUPPORT_MAIL_FROM', "system@beispiel.de");
-
+define('SUPPORT_MAIL_FROM', "system@kunde.de");
 
 // Der Salt wird zum Speichern der Passwort Hashes in der Datenbank verwendet und mach den Hash etwas sicherer.
 define('PASSWORD_SALT', 'abc');
